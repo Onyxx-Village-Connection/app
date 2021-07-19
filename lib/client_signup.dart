1d2d822e242ccf200e11f2d27dfa1a35cb1d264b
Alyssa Kongswangwongsa <zazara60@gmail.com>
2021-07-19 15:34:08 +0700

    add logo pic to use for the app

--- lib/client_signup.dart
+++ lib/client_signup.dart
@@ -6,6 +6,12 @@ class ClientSignup extends StatelessWidget{
   const  ClientSignup();
 
   Widget _buildClientSignupWidgets(BuildContext context){
+    return ListView(
+      children: <Widget> [
+
+
+      ],
+    );
   }
 
   void _navigateToDonatingFood(BuildContext context){
