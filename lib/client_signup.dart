10fcafd8da5245c8789a5bdee84592b1ecf08b32
Alyssa Kongswangwongsa <zazara60@gmail.com>
2021-07-19 15:02:22 +0700

    add skeleton code for client signup page

---
+++ lib/client_signup.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+final _backgroundColor = Colors.black87;
+
+class ClientSignup extends StatelessWidget{
+  const  ClientSignup();
+
+  Widget _buildClientSignupWidgets(BuildContext context){
+  }
+
+  void _navigateToDonatingFood(BuildContext context){
+
+  }
+
+  @override
+  Widget build(BuildContext context){
+
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ovcapp/client_signup.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,14 +10,12 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'OVC App',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: Scaffold(
-        appBar: AppBar(),
-        body: Text('OVC App Placeholder'),
-      ),
+      home: ClientSignup(),
     );
   }
 }
