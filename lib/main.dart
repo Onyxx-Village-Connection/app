18fd84a53625add07c094631b52a21d39d32b94a
Michael Fang <lfang.michael@gmail.com>
2021-07-24 18:16:29 -0700

    add landing page

--- lib/main.dart
+++ lib/main.dart
@@ -10,12 +10,45 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'OVC App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
+      theme: ThemeData.dark(),
       home: Scaffold(
-        appBar: AppBar(),
-        body: Text('OVC App Placeholder'),
+          appBar: AppBar(),
+          body: Center(
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Padding(
+                  padding: const EdgeInsets.all(10.0),
+                  child: Image.asset(
+                    'images/OVC-logo-cropped.jpg',
+                  ),
+                ),
+                roleButton('Donor'),
+                roleButton('Volunteer'),
+                roleButton('client'),
+              ],
+            ),
+          )),
+    );
+  }
+
+  Padding roleButton(String role) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: TextButton(
+        onPressed: () {
+          print('${role} click');
+        },
+        child: Text(
+          'I am a ${role}',
+        ),
+        style: TextButton.styleFrom(
+          backgroundColor: Colors.white,
+          primary: Colors.black,
+          padding: EdgeInsets.all(5.0),
+          minimumSize: Size(200, 40),
+          textStyle: const TextStyle(fontSize: 24),
+        ),
       ),
     );
   }
