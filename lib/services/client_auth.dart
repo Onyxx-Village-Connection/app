8b4eeb18f34f07dc6fc9ed8e046aa8164906fb62
Alyssa Kongswangwongsa <zazara60@gmail.com>
2021-08-10 21:01:19 +0700

    create client_auth file for authentication service

---
+++ lib/services/client_auth.dart
@@ -0,0 +1,12 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class AuthService{
+
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  //sign in w/ email n' password
+
+  //register w/ email n' password
+
+  //sign out
+
+}
