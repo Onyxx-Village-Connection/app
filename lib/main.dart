76a68f4bf89ef12464220c466c41c48639ecf77e
Alyssa Kongswangwongsa <zazara60@gmail.com>
2021-08-11 16:47:56 +0700

    another fail attempt on trying to integrate firebase

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
+      title: 'OVC app',
       home: ClientLogin(title: 'Client Login',),
     );
   }
