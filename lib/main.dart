55a2f36c4696902acf679af1684733833a6c6e29
aysuluu <aysuluuw@google.com>
2021-08-18 16:16:05 -0700

    initialize FlutterFire

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,9 @@ import 'package:ovcapp/screens/authenticate/client_signup.dart';
 import 'package:ovcapp/landing_page.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
