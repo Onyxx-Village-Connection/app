cf6aa81c7206b0626a1bb9de6f73c6353c8cdf8e
Aladdin Ismael <ismael.aladdin@gmail.com>
2021-08-28 02:07:59 -0700

    Clean up and add comments regarding location tracking plan

---
+++ lib/screens/map/datamodels/userdata.dart
@@ -0,0 +1,9 @@
+class Volunteer {
+  String name = 'unknown';
+  String itemName = 'unknown';
+  String jobType = 'unknown';
+  bool isOnline = false;
+  //double latitude = 37.466089;
+  //double longitude = -122.156659;
+  String geoHash = '';
+}
