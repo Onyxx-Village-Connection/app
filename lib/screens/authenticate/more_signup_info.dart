91e74c84ddd705f9ce7e3ee6d95036e08aee155d
aysuluu <aysuluuw@google.com>
2021-08-30 03:30:12 -0700

    save new user info to firebase

--- lib/screens/authenticate/more_signup_info.dart
+++ lib/screens/authenticate/more_signup_info.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:ovcapp/screens/client/client_wishlist.dart';
 
@@ -14,6 +15,12 @@ class MoreSignupInfo extends StatefulWidget{
 }
 
 class _MoreSignupInfoState extends State<MoreSignupInfo>{
+
+
+  String name = '';
+  String phone = '';
+  String city = '';
+  String howLong = '';
 
   final _auth = FirebaseAuth.instance;
 
@@ -51,6 +58,9 @@ class _MoreSignupInfoState extends State<MoreSignupInfo>{
           return 'Please enter your name';
         }
       },
+      onChanged: (val){
+        setState(() => name = val);
+      },
     );
 
     final phoneNumberBox = TextFormField(
@@ -66,6 +76,9 @@ class _MoreSignupInfoState extends State<MoreSignupInfo>{
         if (value == null || value.isEmpty){
           return 'Please enter your phone number';
         }
+      },
+      onChanged: (val){
+        setState(() => phone = val);
       },
     );
 
@@ -83,6 +96,9 @@ class _MoreSignupInfoState extends State<MoreSignupInfo>{
           return 'Please enter your city';
         }
       },
+      onChanged: (val){
+        setState(() => city = val);
+      },
     );
 
     final howLongBox = TextFormField(
@@ -99,6 +115,9 @@ class _MoreSignupInfoState extends State<MoreSignupInfo>{
           return 'Please enter the length of time you have been involved';
         }
       },
+      onChanged: (val){
+        setState(() => howLong = val);
+      },
     );
 
     final signupButton = Material(
@@ -109,6 +128,8 @@ class _MoreSignupInfoState extends State<MoreSignupInfo>{
         padding: EdgeInsets.fromLTRB(30.0, 20.0, 30.0, 20.0),
         onPressed: () async {
           await _auth.createUserWithEmailAndPassword(email: widget.email, password: widget.password).then((_){
+            User? client = _auth.currentUser;
+            clientSetup(name, phone, city, howLong);
             Navigator.of(context).pushReplacement(MaterialPageRoute(builder: (context) => ClientWishlist(title: 'Client Wishlist')));
           });
           },
@@ -152,6 +173,14 @@ class _MoreSignupInfoState extends State<MoreSignupInfo>{
         ),
       ],
     );
+  }
+
+  Future<void> clientSetup(String name, String phone, String city, String howLong) async {
+    CollectionReference clients = FirebaseFirestore.instance.collection('Clients');
+    FirebaseAuth auth = FirebaseAuth.instance;
+    String uid = auth.currentUser!.uid.toString();
+    clients.add({'uid': uid, 'name': name, 'phone': phone, 'city': city, 'how long with OVC': howLong});
+    return;
   }
 
   @override
